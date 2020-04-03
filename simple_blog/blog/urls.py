155005b892fe698b6afee995fa68b68af69178f7
Fleskimiso <dawid82228@o2.pl>
2020-04-03 23:18:56 +0200

    added new functionalities and css

--- simple_blog/blog/urls.py
+++ simple_blog/blog/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
 	path('login/', views.login, name='login'),
 	path('logout/',views.logout,name='logout'),
 	path('thread/<str:title>/',views.thread,name='thread'),
-	path('thread/posts/<int:identification>', views.forum_post, name='post' )
+	path('thread/<str:title>/create_post/',views.create_post,name='create_post'),
+	path('thread/posts/<int:identification>/', views.forum_post, name='post' ),
+	path('thread/posts/comment',views.create_comment , name='create_comment' ),
+	path('thread/posts/<int:identification>/delete/',views.delete_post, name='post_delete')
 ]
--- simple_blog/blog/views.py
+++ simple_blog/blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User, UserManager
 from django.contrib.auth import authenticate, login as LOGIN, logout as LOGOUT
 from django.core.exceptions import ObjectDoesNotExist
 
-from .models import Thread, User as Forum_user , Post
+from .models import Thread, User as Forum_user , Post, Comment
 import datetime
 # Create your views here.
 def forum(request):
@@ -53,4 +53,36 @@ def thread(request,title):
 
 def forum_post(request,identification):
 	post = Post.objects.get(pk=identification)
-	return render(request,'blog/post.html',{"post" : post })
+	comments = Comment.objects.filter(post = post)
+	print(comments)
+	return render(request,'blog/post.html',{"post" : post , "comments": comments})
+
+def delete_post(request,identification):
+	post = Post.objects.get(pk = request.POST['post_id'])
+	post_thread = post.thread.title
+	Post.objects.remove(post)
+	return redirect('blog:thread', post_thread);
+def create_post(request,title):
+		if request.method == 'GET':
+			return render(request,'blog/create_post_template.html', {'title' : title})
+		elif request.method == 'POST':
+			title_post = request.POST['title_post']
+			content = request.POST['content']
+			author = Forum_user.objects.get(credentials = request.user)
+			thread = Thread.objects.get(title=title)
+			new_post = Post(title =title_post, content = content, author = author, thread=thread);
+			new_post.save() #to do tests
+			return redirect('blog:thread',title);
+
+def create_comment(request): #TOBE TESTED 
+	if request.method == 'POST':
+		if request.user.is_authenticated == True:
+			post_id = request.POST['post_id']
+			comment_content = request.POST['content']
+			user = Forum_user.objects.get(credentials__id = request.user.id)
+			post_instance = Post.objects.get(pk=post_id)
+			comment = Comment(comment_text=comment_content,author=user,post=post_instance)
+			comment.save()
+			return redirect('blog:post', post_id)
+	elif request.method == 'GET':
+		return redirect('blog:forum')
